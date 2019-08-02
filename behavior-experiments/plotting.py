9e15745b3af3564dbb2ffbf42e7d7914809bd034
Sebastien Maille <smail031@uottawa.ca>
2019-08-02 11:02:10 -0400

    Basic and complex changes

--- behavior-experiments/plotting.py
+++ behavior-experiments/plotting.py
@@ -13,7 +13,7 @@ import matplotlib.gridspec as gridspec
 
 filename = input('Filename: ')
 #trial = input('Trial: ')
-num_trials = 5
+num_trials = 15
 
 f = h5py.File(filename, 'r')
 
@@ -31,28 +31,26 @@ for ind, key in enumerate(lick_keys):
 
     ax.append(fig.add_subplot(gs[0, ind]))
     for trial in range(num_trials):
-        
-        _lick_v = f[key]['volt'] [int(trial)-1]
+
+        _lick_v = f[key]['volt'] [trial]
         _d_lick_v = np.diff(_lick_v)
         _licks = np.argwhere(_d_lick_v > 0).flatten()
         _values = np.full(len(_licks), trial)
-        
         _tone_on = f['tone']['t'][trial]
-        ax[ind].fill_between([_tone_on, _tone_on + 1000], [trial-0.5, trial-0.5], 
+        ax[ind].fill_between([_tone_on, _tone_on + 1000], [trial-0.5, trial-0.5],
           [trial+0.5, trial+0.5],
           facecolor = '#ff0000', alpha = 0.2)
         ax[ind].scatter(_licks, _values, marker = '|', color = '#5d5d5d', s = 40)
 
-        
-    
-    ax[ind].set_ylim([0,num_trials])
+    ax[ind].set_ylim([-1,num_trials]+1)
     ax[ind].set_xlim([0, None])
 #    ax[ind].set_xticks([0, 1000, 2000, 3000, 4000, 5000])
     ax[ind].set_xlabel('Time (ms)')
     ax[ind].set_ylabel('Trials')
 
-    
+
     _title_str = key[-1].upper() + ' Licks'
     ax[ind].set_title(_title_str)
 
+plt.show(block=True)
 #fig.savefig('test.pdf')
