1e1928e9cbff5e3dc63af84cc1e6ac33c0259782
Sebastien Maille <smail031@uottawa.ca>
2019-08-08 13:53:52 -0400

    Inverted image and made preview window smaller

--- behavior-experiments/picam.py
+++ behavior-experiments/picam.py
@@ -3,6 +3,6 @@ import time
 
 camera = PiCamera()
 
-camera.start_preview()
+camera.start_preview(rotation = 180, fullscreen = False, window = (0,-44,350,400))
 time.sleep(15)
 camera.stop_preview()
