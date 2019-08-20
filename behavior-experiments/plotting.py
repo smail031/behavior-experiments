307e085a9c37627df62e65bfb3938c914e7695aa
Sebastien Maille <smail031@uottawa.ca>
2019-08-20 12:12:28 -0400

    Fixed plotting of lick times and added a bar on the side of each trial indicating whether the response was correct, incorrect or null

--- behavior-experiments/plotting.py
+++ behavior-experiments/plotting.py
@@ -16,6 +16,7 @@ import time
 mouse_number = input('Mouse: ')
 date_experiment = input('Date of experiment (yyyy-mm-dd or today): ')
 block_number = input('Block number: ')
+x_limit = 5000
 
 if date_experiment == 'today':
     date_experiment = time.strftime("%Y-%m-%d", time.localtime(time.time()))
@@ -38,11 +39,30 @@ ax = []
 
 #transform trial types from "L/R" to "0/1" to simplify plotting
 trial_types = []
+
+correct_response = np.zeros(num_trials)
+correct_response.fill(np.nan)
+incorrect_response = np.zeros(num_trials)
+incorrect_response.fill(np.nan)
+null_response = np.zeros(num_trials)
+null_response.fill(np.nan)
+
 for trial in range(num_trials):
     if 'L' in str(f['sample_tone']['type'][trial]):
         trial_types.append(0)
     elif 'R' in str(f['sample_tone']['type'][trial]):
         trial_types.append(1)
+
+    if str(f['response'][trial]) == str(f['sample_tone']['type'][trial]):
+        correct_response[trial] = x_limit
+    elif 'N' in str(f['response'][trial]):
+        null_response[trial] = x_limit
+    else:
+        incorrect_response[trial] = x_limit
+
+correct_response = np.expand_dims(correct_response, 1)
+incorrect_response = np.expand_dims(incorrect_response, 1)
+null_response = np.expand_dims(null_response, 1)
 
 for ind, key in enumerate(sides):
 
@@ -55,7 +75,12 @@ for ind, key in enumerate(sides):
         _d_lick_v = np.diff(_lick_v) #1st derivative of raw lick data
         _licks = np.argwhere(_d_lick_v > 0).flatten() #find indices where the
                                     #derivative of raw lick data goes above 0
-        _all_licks.append(_licks)
+        _lick_timestamps = []
+
+        if len(_licks) != 0:
+            for i in range(len(_licks)):
+                _lick_timestamps.append(f[f'lick_{key}']['t'][trial][_licks[i]])
+        _all_licks.append(_lick_timestamps)
 
         if len(trial_types) != 0 and trial_types[trial] == ind:
             ax[ind].fill_between([0,5000], [trial-0.5, trial-0.5],
@@ -78,14 +103,21 @@ for ind, key in enumerate(sides):
         plt.plot([go_tone_end, go_tone_end], [trial-0.5, trial+0.5,], '#808080', lw = 0.5)
 
 
-    plt.eventplot(_all_licks, linelengths=0.8, linewidths = 0.8, colors = 'black') #creates raster
+
+
+    plt.eventplot(_all_licks, linelengths = 0.8, linewidths = 0.6, colors = '#6e6e6e') #creates raster
     reward_times = np.expand_dims(f[f'rew_{key}']['t'], 1)
-    plt.eventplot(reward_times, linelengths=1, colors = '#00c907')
+
+    plt.eventplot(reward_times, linelengths = 0.8, linewidths = 0.9, colors = 'black')
+
+    plt.eventplot(correct_response, colors = 'green', linewidths = 5)
+    plt.eventplot(incorrect_response, colors = 'red', linewidths = 5)
+    plt.eventplot(null_response, colors = '#ffcc00', linewidths = 5)
 
     #set axis limits, labels and title.
     ax[ind].set_ylim([0,num_trials])
-    ax[ind].set_xlim([0, 4000])
-    ax[ind].set_xticks([0, 1000, 2000, 3000, 4000])
+    ax[ind].set_xlim([0, x_limit])
+    ax[ind].set_xticks([0, 1000, 2000, 3000, 4000, 5000])
     ax[ind].set_xlabel('Time (ms)')
     ax[ind].set_ylabel('Trials')
     ax[ind].spines['top'].set_visible(False)
