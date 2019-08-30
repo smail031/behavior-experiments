52ab45f7fcf22524df3a35ad18764f5407a780a9
Sebastien Maille <smail031@uottawa.ca>
2019-08-30 17:36:15 -0400

    Changed delay length and reward size

--- behavior-experiments/operant2.py
+++ behavior-experiments/operant2.py
@@ -28,7 +28,7 @@ mouse_number = input('mouse number: ' ) #asks user for mouse number
 block_number = input('block number: ' ) #asks user for block number (for file storage)
 n_trials = int(input('How many trials?: ' )) #number of trials in this block
 
-delay_length = 0 #length of delay between sample tone and go cue, in sec
+delay_length = 0.2 #length of delay between sample tone and go cue, in sec
 response_delay = 2000 #length of time for animals to give response
 
 L_tone_freq = 1000 #frequency of sample tone in left lick trials
@@ -41,7 +41,7 @@ wrong_tone_length = 1
 go_tone_freq = 500 #frequency of go tone
 go_tone_length = 0.1
 
-reward_size = 5 #size of water rewards in uL
+reward_size = 2 #size of water rewards in uL
 
 #----------------------------
 #Assign GPIO pins:
