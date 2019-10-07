55e6d7e02bd3730d2c321769094472f727173c9c
Sebastien Maille <smail031@uottawa.ca>
2019-10-07 12:43:39 -0400

    adjusted reward sizes

--- behavior-experiments/operant2.py
+++ behavior-experiments/operant2.py
@@ -41,7 +41,7 @@ wrong_tone_length = 1
 go_tone_freq = 500 #frequency of go tone
 go_tone_length = 0.1
 
-reward_size = 2 #size of water rewards in uL
+reward_size = 2.3 #size of water rewards in uL
 
 #----------------------------
 #Assign GPIO pins:
