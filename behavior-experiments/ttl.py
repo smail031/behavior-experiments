1ca812c8922aabc99c406c9dd50863786d530fc3
Sebastien Maille <smail031@uottawa.ca>
2020-01-17 14:22:10 -0500

    troubleshooting

--- behavior-experiments/ttl.py
+++ behavior-experiments/ttl.py
@@ -5,12 +5,14 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-ttlPIN = 3
+ttlPIN = 40
 
-pulse_length = 0.1
+pulse_length = 5
 
 ttl_output = core.ttl(ttlPIN)
-
+print('low')
+time.sleep(5)
+print('high')
 ttl_output.pulse(pulse_length)
 
 for i in range(10):
