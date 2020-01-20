25d9a5487a734d17d3e69ded9f5747607f01a6d4
Sebastien Maille <smail031@uottawa.ca>
2020-01-20 15:12:42 -0500

    updated ttl.py for receiving TTL pulse from the microscope.

--- behavior-experiments/ttl.py
+++ behavior-experiments/ttl.py
@@ -4,16 +4,30 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-ttlPIN = 21
+ttl_out_PIN = 21
+ttl_in_PIN
 pulse_length = 1
+pulse_received = False
 
-GPIO.setup(ttlPIN, GPIO.OUT)
-GPIO.output(ttlPIN, False)
+GPIO.setup(ttl_in_PIN, GPIO.IN)
+GPIO.setup(ttl_out_PIN, GPIO.OUT)
+GPIO.output(ttl_out_PIN, False)
 
 go = input('ready?')
+
+start_time = time.time()
 
 GPIO.output(ttlPIN, True)
 time.sleep(pulse_length)
 GPIO.output(ttlPIN, False)
 
-print('done')
+sent_time = time.time() - start_time
+
+while pulse_received == False:
+    if GPIO.input(ttl_in_PIN):
+        pulse_received == True
+
+received_time = time.time() - start_time
+
+print(sent_time)
+print(received_time)
