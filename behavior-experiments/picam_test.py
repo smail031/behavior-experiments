0ce361957903ad243aa231f4dea228df500375ad
Sebastien Maille <smail031@uottawa.ca>
2020-01-21 17:02:14 -0500

    Added preview to see if it works at the same time as recording

--- behavior-experiments/picam_test.py
+++ behavior-experiments/picam_test.py
@@ -1,8 +1,10 @@
 import picamera
 
 camera = picamera.PiCamera()
+camera.start_preview(rotation = 180, fullscreen = False, window = (0,-44,350,400))
 camera.resolution = (640, 480)
 camera.rotation = (180)
 camera.start_recording('my_video.mjpeg')
 camera.wait_recording(10)
 camera.stop_recording()
+camera.stop_preview()
