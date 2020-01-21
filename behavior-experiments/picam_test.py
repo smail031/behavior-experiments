b6548c4adffa26affebc9db1e115343441b98e97
Sebastien Maille <smail031@uottawa.ca>
2020-01-21 16:23:17 -0500

    testing picam recording

--- behavior-experiments/picam_test.py
+++ behavior-experiments/picam_test.py
@@ -2,6 +2,7 @@ import picamera
 
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
+camera.rotation = (180)
 camera.start_recording('my_video.h264')
 camera.wait_recording(10)
 camera.stop_recording()
