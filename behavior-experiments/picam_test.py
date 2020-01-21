368f7613f38077d7424ceeea4f6b27257da818bb
Sebastien Maille <smail031@uottawa.ca>
2020-01-21 16:29:25 -0500

    trying mjpeg

--- behavior-experiments/picam_test.py
+++ behavior-experiments/picam_test.py
@@ -3,6 +3,6 @@ import picamera
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
 camera.rotation = (180)
-camera.start_recording('my_video.h264')
+camera.start_recording('my_video.mjpeg')
 camera.wait_recording(10)
 camera.stop_recording()
