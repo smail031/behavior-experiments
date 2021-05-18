ed11a38dba1e4d2e808c43a4fc7859989ad43829
smail031 <smail031@uottawa.ca>
2021-05-18 10:09:03 -0400

    Printing rule as int

--- behavior-experiments/set_shifting_prob.py
+++ behavior-experiments/set_shifting_prob.py
@@ -153,8 +153,8 @@ if yesterday == 'y':
 
     f = h5py.File(yesterday_file, 'r') #open HDF5 file
     
-    freq_rule = f['rule']['freq_rule'][-1] #get value of freq_rule of last trial yesterday
-    left_port = f['rule']['left_port'][-1] #get value of left_port of last trial yesterday
+    freq_rule = int(f['rule']['freq_rule'][-1]) #get value of freq_rule of last trial yesterday
+    left_port = int(f['rule']['left_port'][-1]) #get value of left_port of last trial yesterday
 
 print(f'Rule = [{freq_rule},{left_port}]')
 
