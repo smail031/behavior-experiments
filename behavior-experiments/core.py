99e333f62851efeb37cfca4a09e3f5fc3b2dd8c4
smail031 <smail031@uottawa.ca>
2021-07-16 11:44:20 -0400

    changing water delivery volume

--- behavior-experiments/core.py
+++ behavior-experiments/core.py
@@ -323,7 +323,7 @@ class stepper():
         self.Disable() #disable stepper (to prevent overheating)
 
     def Reward(self,):
-        steps = 200 #Calculate the number of steps needed to deliver
+        steps = 400 #Calculate the number of steps needed to deliver
                                 #volume. 400 steps gives 8.2uL
         if GPIO.input(self.emptyPIN):
             self.Motor(1, steps) #push syringe for "steps" until the empty pin
