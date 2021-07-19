5b6e2d4793b742034823f978d780255942901563
smail031 <smail031@uottawa.ca>
2021-07-19 09:55:24 -0400

    commented annotation and recording code and made the test length indefinite

--- behavior-experiments/test_scripts/picam_test.py
+++ behavior-experiments/test_scripts/picam_test.py
@@ -3,18 +3,17 @@ import time
 
 camera = picamera.PiCamera()
 camera.start_preview(rotation = 0, fullscreen = False, window = (0,-44,350,400))
-camera.resolution = (640, 480)
-camera.rotation = (180)
-camera.annotate_foreground = Color('white')
-camera.annotate_background = Color('black')
+#camera.resolution = (640, 480)
+#camera.rotation = (180)
+#camera.annotate_foreground = Color('white')
+#camera.annotate_background = Color('black')
 
-
-camera.start_recording('my_video.h264')
-camera.annotate_text = "first part"
-time.sleep(5)
-
-camera.annotate_text = "second part"
-time.sleep(5)
-
-camera.stop_recording()
-camera.stop_preview()
+while True:
+    #camera.start_recording('my_video.h264')
+    #camera.annotate_text = "first part"
+    time.sleep(5)
+    
+    #camera.annotate_text = "second part"
+    
+    #camera.stop_recording()
+#camera.stop_preview()
