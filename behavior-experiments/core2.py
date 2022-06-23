d4d023b573aab5c836cb0516758ffb1df4625b19
Sébastien Maillé <smail031@uottawa.ca>
2022-06-23 16:49:21 -0400

    Troubleshooting

--- behavior-experiments/core2.py
+++ behavior-experiments/core2.py
@@ -23,7 +23,7 @@ class Trial():
         '''
         '''
         self.data = {}
-        self.data['name'] = name
+        self.data['name'] = self.name
         self.data['n_trials'] = self.n_trials
         self.data['trial_start_time'] = np.empty(self.n_trials, dtype=float)
         self.data['trial_end_time'] = np.empty(self.n_trials, dtype=float)
