7870c030fdb2effd347f560e09973e2a3e04afdc
jenn-h99 <whsie041@uottawa.ca>
2023-03-06 11:14:45 -0500

    update

--- behavior-experiments/operant_var_prob2.py
+++ behavior-experiments/operant_var_prob2.py
@@ -125,7 +125,7 @@ rule = core.ProbSwitchRule([highfreq, lowfreq], left_port, p_index, criterion,
 
 if ttl_experiment == 'y':
     # Set up ttl class instances opto stim TTL output
-    TTL_opto = core.ttl(TTL_opto_PIN, 0.01, 0.04, 2.00)
+    TTL_opto.ttl_pulse = core.ttl(TTL_opto_PIN, 0.01, 0.04, 2.00)
 
 # ------------------------------------------------------------------------------
 # Initialize experiment:
